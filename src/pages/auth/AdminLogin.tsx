d01fc01e48f63c774461fcffea311c856aa9e329
Dc Valerian <Dc-Valerian>
2023-11-29 15:04:03 +0100

    added

--- src/pages/auth/AdminLogin.tsx
+++ src/pages/auth/AdminLogin.tsx
@@ -1,42 +1,41 @@
-import React from 'react'
-import { IoSearchSharp } from "react-icons/io5";
 import { TbLockCog } from "react-icons/tb";
-
 
 const AdminLogin = () => {
   return (
     <div className="w-full h-[100vh] bg-white  flex justify-center items-center">
-<div className="w-[500px] h-[500px] bg-[#F2F4F6] flex justify-center items-center flex-col rounded-[10px] gap-3 "> 
-
-<div className="w-[370px] h-[100px]   flex  flex-col">
-<p className="font-bold text-black text-medium">EMAIL ADDRESS</p>
-          <input type="text " placeholder="Email" className="w-[370px] h-[50px] bg-[#ffff] rounded-[10px] border-solid border-2 border-sky-500 focus:border border-[lightgrey] focus:outline-none"
-
+      <div className="w-[500px] h-[500px] bg-[#F2F4F6] flex justify-center items-center flex-col rounded-[10px] gap-3 ">
+        <div className="w-[370px] h-[100px]   flex  flex-col">
+          <p className="font-bold text-black text-medium">EMAIL ADDRESS</p>
+          <input
+            type="text "
+            placeholder="Email"
+            className="w-[370px] h-[50px] bg-[#ffff] rounded-[10px] border-solid border-2 border-sky-500 focus:border border-[lightgrey] focus:outline-none"
           />
-         
-          </div>
+        </div>
 
-          <div className="w-[370px] h-[90px]  flex flex-col ">
+        <div className="w-[370px] h-[90px]  flex flex-col ">
           <p className="font-bold text-black text-medium">PASSWORD</p>
-          <input type="text " placeholder="Enter Password" className="w-[370px] h-[50px] bg-[#ffff] rounded-[10px] border-solid border-2 border-sky-500 focus:border border-[lightgrey] focus:outline-none"
-
+          <input
+            type="text "
+            placeholder="Enter Password"
+            className="w-[370px] h-[50px] bg-[#ffff] rounded-[10px] border-solid border-2 border-sky-500 focus:border border-[lightgrey] focus:outline-none"
           />
           <div className="flex absolute ml-[330px] top-[49%] ">
-        
-            <TbLockCog  style={{ color: 'grey', fontSize: '20px' }} />
-  
+            <TbLockCog style={{ color: "grey", fontSize: "20px" }} />
           </div>
-          </div>
+        </div>
 
-<div className="w-[380px] h-[50px] flex justify-between items-center">
-    <p className="font-bold text-black">Remember Me</p>
-    <p className="font-bold text-black">Forget Password</p>
-</div>
+        <div className="w-[380px] h-[50px] flex justify-between items-center">
+          <p className="font-bold text-black">Remember Me</p>
+          <p className="font-bold text-black">Forget Password</p>
+        </div>
 
-<button className="w-[200px] h-[40px] bg-sky-500 text-white font-bold rounded-[10px] text-center">Log in</button>
-</div>
+        <button className="w-[200px] h-[40px] bg-sky-500 text-white font-bold rounded-[10px] text-center">
+          Log in
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminLogin
+export default AdminLogin;
