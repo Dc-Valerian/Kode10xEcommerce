b2b34bf22b5d3bac3428a77a7268da90ce95af02
Dc Valerian <Dc-Valerian>
2023-12-01 14:08:34 +0100

    remove vercel error

--- src/APIS/Store.tsx
+++ src/APIS/Store.tsx
@@ -2,7 +2,6 @@ import myReducer from "./ReduxState";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import {
-  persistStore,
   persistReducer,
   FLUSH,
   REHYDRATE,
@@ -12,7 +11,6 @@ import {
   REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { PersistGate } from "redux-persist/integration/react";
 
 const persistConfig = {
   key: "Ecommerce",
