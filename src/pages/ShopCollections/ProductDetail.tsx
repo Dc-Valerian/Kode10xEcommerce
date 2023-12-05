a1283fcbf467fb57a747801de45165bdda7cb08d
Dc Valerian <Dc-Valerian>
2023-12-05 20:48:14 +0100

    added

--- src/pages/ShopCollections/ProductDetail.tsx
+++ src/pages/ShopCollections/ProductDetail.tsx
@@ -1,36 +1,133 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 
-interface Product {
-  imageUrl: string;
-  name: string;
-  price: number;
-  discountedPrice: number;
-}
-
-export const ProductDetail: React.FC<{ product: Product }> = ({ product }) => {
-  const { imageUrl, name, price, discountedPrice } = product;
-
+export const ProductDetail = () => {
   return (
-    <div className="w-[300px] h-[350px] flex justify-center items-center flex-col">
-      <NavLink to={`/product-details`} style={{ textDecoration: "none" }}>
-        <div className="w-[300px] h-[300px] bg-[#F2F4F6] flex justify-center items-center">
-          <img
-            src={imageUrl}
-            alt={name}
-            className="w-[350px] h-[300px] cursor-pointer transform transition duration-300 ease-in-out hover:scale-[1.04]"
-          />
-        </div>
-      </NavLink>
-      <div className="w-[350px] h-[170px] flex flex-col justify-center items-center">
-        <p className="text-center text-[grey] font-lighter">{name}</p>
+    <div className=" flex flex-wrap gap-[20px] mt-[10px] items-center justify-center">
+      <div className="w-[300px] h-[350px] flex justify-center items-center flex-col">
+        <NavLink to={`/product-details`} style={{ textDecoration: "none" }}>
+          <div className="w-[300px] h-[300px] bg-[#F2F4F6] flex justify-center items-center">
+            <img
+              src="https://websitedemos.net/shoe-store-04/wp-content/uploads/sites/247/2021/03/sports-shoe5-600x600.jpg"
+              className="w-[350px] h-[300px] cursor-pointer transform transition duration-300 ease-in-out hover:scale-[1.04]"
+            />
+          </div>
+        </NavLink>
+        <div className="w-[350px] h-[170px] flex flex-col justify-center items-center">
+          <p className="text-center text-[grey] font-lighter">Rolex</p>
 
-        <p className="text-center text-[#00000096] font-semi-bold">
-          ${price}{" "}
-          <span className="text-center font-bold text-[#000000b9]">
-            ${discountedPrice}
-          </span>{" "}
-        </p>
+          <p className="text-center text-[#00000096] font-semi-bold">
+            $404{" "}
+            <span className="text-center font-bold text-[#000000b9]">$20</span>{" "}
+          </p>
+        </div>
+      </div>
+      <div className="w-[300px] h-[350px] flex justify-center items-center flex-col">
+        <NavLink to={`/product-details`} style={{ textDecoration: "none" }}>
+          <div className="w-[300px] h-[300px] bg-[#F2F4F6] flex justify-center items-center">
+            <img
+              src="https://websitedemos.net/shoe-store-04/wp-content/uploads/sites/247/2021/03/sports-shoe5-600x600.jpg"
+              className="w-[350px] h-[300px] cursor-pointer transform transition duration-300 ease-in-out hover:scale-[1.04]"
+            />
+          </div>
+        </NavLink>
+        <div className="w-[350px] h-[170px] flex flex-col justify-center items-center">
+          <p className="text-center text-[grey] font-lighter">Rolex</p>
+
+          <p className="text-center text-[#00000096] font-semi-bold">
+            $404{" "}
+            <span className="text-center font-bold text-[#000000b9]">$20</span>{" "}
+          </p>
+        </div>
+      </div>
+      <div className="w-[300px] h-[350px] flex justify-center items-center flex-col">
+        <NavLink to={`/product-details`} style={{ textDecoration: "none" }}>
+          <div className="w-[300px] h-[300px] bg-[#F2F4F6] flex justify-center items-center">
+            <img
+              src="https://websitedemos.net/shoe-store-04/wp-content/uploads/sites/247/2021/03/sports-shoe5-600x600.jpg"
+              className="w-[350px] h-[300px] cursor-pointer transform transition duration-300 ease-in-out hover:scale-[1.04]"
+            />
+          </div>
+        </NavLink>
+        <div className="w-[350px] h-[170px] flex flex-col justify-center items-center">
+          <p className="text-center text-[grey] font-lighter">Rolex</p>
+
+          <p className="text-center text-[#00000096] font-semi-bold">
+            $404{" "}
+            <span className="text-center font-bold text-[#000000b9]">$20</span>{" "}
+          </p>
+        </div>
+      </div>
+      <div className="w-[300px] h-[350px] flex justify-center items-center flex-col">
+        <NavLink to={`/product-details`} style={{ textDecoration: "none" }}>
+          <div className="w-[300px] h-[300px] bg-[#F2F4F6] flex justify-center items-center">
+            <img
+              src="https://websitedemos.net/shoe-store-04/wp-content/uploads/sites/247/2021/03/sports-shoe5-600x600.jpg"
+              className="w-[350px] h-[300px] cursor-pointer transform transition duration-300 ease-in-out hover:scale-[1.04]"
+            />
+          </div>
+        </NavLink>
+        <div className="w-[350px] h-[170px] flex flex-col justify-center items-center">
+          <p className="text-center text-[grey] font-lighter">Rolex</p>
+
+          <p className="text-center text-[#00000096] font-semi-bold">
+            $404{" "}
+            <span className="text-center font-bold text-[#000000b9]">$20</span>{" "}
+          </p>
+        </div>
+      </div>
+      <div className="w-[300px] h-[350px] flex justify-center items-center flex-col">
+        <NavLink to={`/product-details`} style={{ textDecoration: "none" }}>
+          <div className="w-[300px] h-[300px] bg-[#F2F4F6] flex justify-center items-center">
+            <img
+              src="https://websitedemos.net/shoe-store-04/wp-content/uploads/sites/247/2021/03/sports-shoe5-600x600.jpg"
+              className="w-[350px] h-[300px] cursor-pointer transform transition duration-300 ease-in-out hover:scale-[1.04]"
+            />
+          </div>
+        </NavLink>
+        <div className="w-[350px] h-[170px] flex flex-col justify-center items-center">
+          <p className="text-center text-[grey] font-lighter">Rolex</p>
+
+          <p className="text-center text-[#00000096] font-semi-bold">
+            $404{" "}
+            <span className="text-center font-bold text-[#000000b9]">$20</span>{" "}
+          </p>
+        </div>
+      </div>
+      <div className="w-[300px] h-[350px] flex justify-center items-center flex-col">
+        <NavLink to={`/product-details`} style={{ textDecoration: "none" }}>
+          <div className="w-[300px] h-[300px] bg-[#F2F4F6] flex justify-center items-center">
+            <img
+              src="https://websitedemos.net/shoe-store-04/wp-content/uploads/sites/247/2021/03/sports-shoe5-600x600.jpg"
+              className="w-[350px] h-[300px] cursor-pointer transform transition duration-300 ease-in-out hover:scale-[1.04]"
+            />
+          </div>
+        </NavLink>
+        <div className="w-[350px] h-[170px] flex flex-col justify-center items-center">
+          <p className="text-center text-[grey] font-lighter">Rolex</p>
+
+          <p className="text-center text-[#00000096] font-semi-bold">
+            $404{" "}
+            <span className="text-center font-bold text-[#000000b9]">$20</span>{" "}
+          </p>
+        </div>
+      </div>
+      <div className="w-[300px] h-[350px] flex justify-center items-center flex-col">
+        <NavLink to={`/product-details`} style={{ textDecoration: "none" }}>
+          <div className="w-[300px] h-[300px] bg-[#F2F4F6] flex justify-center items-center">
+            <img
+              src="https://websitedemos.net/shoe-store-04/wp-content/uploads/sites/247/2021/03/sports-shoe5-600x600.jpg"
+              className="w-[350px] h-[300px] cursor-pointer transform transition duration-300 ease-in-out hover:scale-[1.04]"
+            />
+          </div>
+        </NavLink>
+        <div className="w-[350px] h-[170px] flex flex-col justify-center items-center">
+          <p className="text-center text-[grey] font-lighter">Rolex</p>
+
+          <p className="text-center text-[#00000096] font-semi-bold">
+            $404{" "}
+            <span className="text-center font-bold text-[#000000b9]">$20</span>{" "}
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/ShopCollections/ShopNow.tsx
+++ src/pages/ShopCollections/ShopNow.tsx
@@ -1,14 +1,10 @@
-import { products } from "./ProductCard";
+
 import { ProductDetail } from "./ProductDetail";
 
 const ShopNow = () => {
   return (
-    <div
-    className="flex flex-wrap items-center justify-center mt-[90px] gap-[15px]"
-    >
-      {products.map((product, index) => (
-        <ProductDetail key={index} product={product} />
-      ))}
+    <div className="flex flex-wrap items-center justify-center mt-[90px] gap-[15px]">
+      <ProductDetail />
     </div>
   );
 };
